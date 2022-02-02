d26678fbb24977953b444c24900bcad309db287c
Tanvir Mahin <tanvirmahin24@gmail.com>
2022-02-02 13:44:52 +0600

    name changed

--- utils/getConfFile.js
+++ utils/getConfFile.js
@@ -1,11 +1,20 @@
 import * as fs from "fs";
 
 const getConfFile = (options) => {
-  const data = fs.readFileSync("config.compgen.json", {
-    encoding: "utf8",
-    flag: "r",
-  });
-  return { ...options, ...JSON.parse(data) };
+  try {
+    if (fs.existsSync("config.compgen.json")) {
+      const data = fs.readFileSync("config.compgen.json", {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      return { ...options, ...JSON.parse(data) };
+    } else {
+      return options;
+    }
+  } catch (err) {
+    return options;
+  }
 };
 
 export default getConfFile;
