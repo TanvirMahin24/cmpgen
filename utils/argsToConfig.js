b5f639e291a1d8a82dd19e6e3d85baa466e1355a
Tanvir Mahin <tanvirmahin24@gmail.com>
2022-02-02 13:26:33 +0600

    --help flag added

--- utils/argsToConfig.js
+++ utils/argsToConfig.js
@@ -1,4 +1,5 @@
 import arg from "arg";
+import chalk from "chalk";
 
 const argsToConfig = () => {
   let defaultConfig = {
@@ -15,11 +16,35 @@ const argsToConfig = () => {
       "--css": String,
       "-i": "--index",
       "-c": "--css",
+      "--help": Boolean,
+      "-h": "--help",
+      "--version": Boolean,
+      "-v": "--version",
     },
     {
       argv: rawArgs.slice(2),
     }
   );
+  if (args["--help"]) {
+    console.log(chalk.bgBlack("Usage:"));
+    console.log("compgen <new-component> [options]");
+    console.log(chalk.green("Options:"));
+    console.log(
+      "  -i, --index     Generate index.js file with component" +
+        chalk.blue("(default: false)")
+    );
+    console.log(
+      "  -c, --css       Generate css file valid options: css, scss, sass" +
+        chalk.blue("(default: css)")
+    );
+    console.log("  -h, --help      Show help");
+    process.exit(0);
+  }
+  if (args["--version"]) {
+    console.log(chalk.green("Version:") + " 0.0.1");
+
+    process.exit(0);
+  }
 
   return {
     index: args["--index"] || defaultConfig.index,
